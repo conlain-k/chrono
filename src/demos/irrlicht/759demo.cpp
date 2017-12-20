2a53d0aeba43d04e3c320e35ccad0f433fef4c7a
Conlain Kelly <conlain.k@gmail.com>
2017-12-20 01:49:37 -0600

    add some decent broadphase, gotta do binning

--- src/demos/irrlicht/759demo.cpp
+++ src/demos/irrlicht/759demo.cpp
@@ -38,6 +38,8 @@ using namespace irr::core;
 using namespace irr::scene;
 using namespace irr::video;
 
+bool useBroadphase = true;
+
 // Callback class for contact processing.
 class ContactManager : public ChContactContainer::ReportContactCallback {
   public:
@@ -95,6 +97,7 @@ typedef struct {
     const std::vector<ChVector<double>> vertices;
     const std::vector<ChVector<double>> normals;
     const std::vector<ChVector<int>> indices;
+    ChBody& mFrame;
     // Store AABB mins (xmin,ymin,zmin)
     double AABBmin[3];
     // Store AABB maxes (xmax,ymax,zmax)
@@ -111,21 +114,45 @@ void copyVectorToPointOffset(const ChVector<>& source, Point& dest) {
     dest[1] = source.y();
     dest[2] = source.z();
 }
-// Create AABB for a mesh
-void mesh_t_compute_AABB(mesh_t& mesh, ChFrame<> frame) {
-    double xmin, xmax, ymin, ymax, zmin, zmax;
-    int ncontacts = 0;
 
-    Point V1, V2, V3;
-    Point U1, U2, U3;
-    ChVector<int> i1, i2;
-    int size = mesh.indices.size();
+// Copy all vertices into points, with frame transform
+void copyVerticesToPoints(mesh_t& mesh) {
+    ChVector<int> i;
+    // Pull into local scope for cache-friendly
+
+    const int size = mesh.indices.size();
+    auto frame = mesh.mFrame;
+    auto points = mesh.mPoints;
+    auto vertices = mesh.vertices;
+
+#pragma omp parallel for private(i)
+    for (int f1 = 0; f1 < size; f1++) {
+        i = mesh.indices[f1];
+
+        // Copy first vertex into V1
+        copyVectorToPointOffset(frame * (vertices[i.x()]), points[i.x()]);
+        copyVectorToPointOffset(frame * (vertices[i.y()]), points[i.y()]);
+        copyVectorToPointOffset(frame * (vertices[i.z()]), points[i.z()]);
+    }
+}
+
+// Create AABB for a mesh
+void mesh_t_compute_AABB(mesh_t& mesh) {
+    double xmin = DBL_MAX, xmax = -DBL_MAX, ymin = DBL_MAX, ymax = -DBL_MAX, zmin = DBL_MAX, zmax = -DBL_MAX;
+
+    // Pull into local scope for cache-friendly
+    const int size = mesh.indices.size();
+    auto points = mesh.mPoints;
+
+    ChVector<int> i1;
+// std::cout << "size is " << size << std::endl;
+#pragma omp parallel for reduction(min : xmin, ymin, zmin), reduction(max : xmax, ymax, zmax) private(i1)
     for (int f1 = 0; f1 < size; f1++) {
         auto i1 = mesh.indices[f1];
-        ChVector<> P1, P2, P3;
-        P1 = frame * (mesh.vertices[i1.x()]);
-        P2 = frame * (mesh.vertices[i1.y()]);
-        P3 = frame * (mesh.vertices[i1.z()]);
+        // Point P1, P2, P3;
+        Point& P1 = points[i1.x()];
+        Point& P2 = points[i1.y()];
+        Point& P3 = points[i1.z()];
         xmin = min(xmin, min(P1[0], min(P2[0], P3[0])));
         ymin = min(ymin, min(P1[1], min(P2[1], P3[1])));
         zmin = min(zmin, min(P1[2], min(P2[2], P3[2])));
@@ -133,25 +160,46 @@ void mesh_t_compute_AABB(mesh_t& mesh, ChFrame<> frame) {
         ymax = max(ymax, max(P1[1], max(P2[1], P3[1])));
         zmax = max(zmax, max(P1[2], max(P2[2], P3[2])));
     }
+    mesh.AABBmin[0] = xmin;
+    mesh.AABBmin[1] = ymin;
+    mesh.AABBmin[2] = zmin;
+    mesh.AABBmax[0] = xmax;
+    mesh.AABBmax[1] = ymax;
+    mesh.AABBmax[2] = zmax;
 }
+
 // Test if 2 AABBs collide
 bool mesh_t_AABB_collide(const mesh_t& m1, const mesh_t& m2) {
-    return (m1.AABBmin[0] >= m2.AABBmax[0] && m1.AABBmax[0] <= m2.AABBmin[0]) &&
-           (m1.AABBmin[1] >= m2.AABBmax[1] && m1.AABBmax[1] <= m2.AABBmin[1]) &&
-           (m1.AABBmin[2] >= m2.AABBmax[2] && m1.AABBmax[2] <= m2.AABBmin[2]);
+    // bool x = (m1.AABBmin[0] <= m2.AABBmax[0] && m1.AABBmax[0] >= m2.AABBmin[0]);
+    // bool y = (m1.AABBmin[1] <= m2.AABBmax[1] && m1.AABBmax[1] >= m2.AABBmin[1]);
+    // bool z = (m1.AABBmin[2] <= m2.AABBmax[2] && m1.AABBmax[2] >= m2.AABBmin[2]);
+    // printf("x,y,z is %d,%d,%d\n", x, y, z);
+    return (m1.AABBmin[0] <= m2.AABBmax[0] && m1.AABBmax[0] >= m2.AABBmin[0]) &&
+           (m1.AABBmin[1] <= m2.AABBmax[1] && m1.AABBmax[1] >= m2.AABBmin[1]) &&
+           (m1.AABBmin[2] <= m2.AABBmax[2] && m1.AABBmax[2] >= m2.AABBmin[2]);
+}
+bool mesh_t_broadphase(mesh_t& mesh1, mesh_t& mesh2) {
+    mesh_t_compute_AABB(mesh1);
+    mesh_t_compute_AABB(mesh2);
+    bool broad = false;
+    if (broad = mesh_t_AABB_collide(mesh1, mesh2)) {
+        std::cout << "broadphase is true" << std::endl;
+    } else {
+        // std::cout << "broadphase is false" << std::endl;
+    }
+    return broad;
 }
 
-int mesh_t_compute_contacts_fast(const mesh_t& mesh1,
-                                 const mesh_t& mesh2,
-                                 const ChFrame<>& frame1,
-                                 const ChFrame<>& frame2) {
+int mesh_t_compute_contacts_fast(mesh_t& mesh1, mesh_t& mesh2) {
+    copyVerticesToPoints(mesh1);
+    copyVerticesToPoints(mesh2);
+    bool broad = mesh_t_broadphase(mesh1, mesh2);
+    if (useBroadphase && !broad) {
+        // std::cout << "exiting early\n";
+        return 0;
+    }
     int ncontacts = 0;
 
-    // Point V1, V2, V3;
-    // Point U1, U2, U3;
-    // This might have a race condition
-    // #pragma omp parallel for reduction(+ : ncontacts) private(V1, V2, V3, U1, U2, U3, i1, i2) shared(mesh1, mesh2,
-    // frame1, frame2) schedule(dynamic)d
     Point* m1Points = mesh1.mPoints;
     Point* m2Points = mesh2.mPoints;
     // So much indirection has to hurt the cache
@@ -159,38 +207,26 @@ int mesh_t_compute_contacts_fast(const mesh_t& mesh1,
     const int s2 = mesh2.indices.size();
     ChVector<int> i1, i2;
 
-#pragma omp parallel for private(i1, i2)
+#pragma omp parallel for reduction(+ : ncontacts) private(i1, i2) schedule(dynamic)
     for (int f1 = 0; f1 < s1; f1++) {
-        i1 = mesh1.indices[f1];
-
-        // Copy first vertex into V1
-        copyVectorToPointOffset(frame1 * (mesh1.vertices[i1.x()]), m1Points[i1.x()]);
-        copyVectorToPointOffset(frame1 * (mesh1.vertices[i1.y()]), m1Points[i1.y()]);
-        copyVectorToPointOffset(frame1 * (mesh1.vertices[i1.z()]), m1Points[i1.z()]);
-    }
-#pragma omp parallel for private(i1, i2)
-    for (int f2 = 0; f2 < s2; f2++) {
-        i2 = mesh2.indices[f2];
-        copyVectorToPointOffset(frame2 * (mesh2.vertices[i2.x()]), m2Points[i2.x()]);
-        copyVectorToPointOffset(frame2 * (mesh2.vertices[i2.y()]), m2Points[i2.y()]);
-        copyVectorToPointOffset(frame2 * (mesh2.vertices[i2.z()]), m2Points[i2.z()]);
-    }
-#pragma omp parallel for reduction(+ : ncontacts) private(i1, i2) collapse(2)
-    for (int f1 = 0; f1 < s1; f1++) {
-        // #pragma omp parallel for reduction(+ : ncontacts) private(U1, U2, U3, i2)
         for (int f2 = 0; f2 < s2; f2++) {
             i1 = mesh1.indices[f1];
             i2 = mesh2.indices[f2];
             // if (NoDivTriTriIsect(U1, U2, U3, V1, V2, V3) == 1) {
-            if (NoDivTriTriIsect(m1Points[i1.x()], m1Points[i1.y()], m1Points[i1.z()], m2Points[i2.x()],
-                                 m2Points[i2.y()], m2Points[i2.z()]) == 1) {
+            if (NoDivTriTriIsect(m2Points[i2.x()], m2Points[i2.y()], m2Points[i2.z()], m1Points[i1.x()],
+                                 m1Points[i1.y()], m1Points[i1.z()]) == 1) {
                 ncontacts++;
                 printf("collision occured between mesh1 at face %d, mesh2 at face %d \n", f1, f2);
             }
         }
     }
+    if (!broad && ncontacts != 0) {
+        printf("broadphase failed\n");
+        exit(1);
+    }
     return ncontacts;
 }
+
 int mesh_t_compute_contacts_faster(const mesh_t& mesh1,
                                    const mesh_t& mesh2,
                                    const ChFrame<>& frame1,
@@ -204,16 +240,7 @@ int mesh_t_compute_contacts_faster(const mesh_t& mesh1,
     // frame1, frame2) schedule(dynamic)d
     Point* m1Points = mesh1.mPoints;
     Point* m2Points = mesh2.mPoints;
-    // So much indirection has to hurt the cache
-// #pragma omp parallel for
-//     for (int f1 = 0; f1 < mesh1.indices.size(); f1++) {
-//         auto i1 = mesh1.indices[f1];
-//
-//         // Copy first vertex into V1
-//         copyVectorToPointOffset(frame1 * (mesh1.vertices[i1.x()]), m1Points[i1.x()]);
-//         copyVectorToPointOffset(frame1 * (mesh1.vertices[i1.y()]), m1Points[i1.y()]);
-//         copyVectorToPointOffset(frame1 * (mesh1.vertices[i1.z()]), m1Points[i1.z()]);
-//     }
+
 #pragma omp parallel for
     for (int f2 = 0; f2 < mesh2.indices.size(); f2++) {
         auto i2 = mesh2.indices[f2];
@@ -262,7 +289,7 @@ int main(int argc, char* argv[]) {
     ChIrrWizard::add_typical_Logo(application.GetDevice());
     ChIrrWizard::add_typical_Sky(application.GetDevice());
     ChIrrWizard::add_typical_Lights(application.GetDevice());
-    ChIrrWizard::add_typical_Camera(application.GetDevice(), irr::core::vector3df(0, 4, 10));
+    ChIrrWizard::add_typical_Camera(application.GetDevice(), irr::core::vector3df(0, 10, 10));
 
     ChTriangleMeshConnected bunnymesh;
     bunnymesh.LoadWavefrontMesh(GetChronoDataFile("bunny.obj"), true, true);
@@ -271,12 +298,6 @@ int main(int argc, char* argv[]) {
     ChTriangleMeshConnected bunnymesh2;
     bunnymesh2.LoadWavefrontMesh(GetChronoDataFile("bunny.obj"), true, true);
     bunnymesh2.Transform({0, 0, 0}, ChMatrix33<>(50.0));
-
-    // In this example the two meshes are the same, the offsets are just different
-    mesh_t mesh1 = {bunnymesh.m_vertices, bunnymesh.m_normals, bunnymesh.m_face_v_indices};
-    mesh1.mPoints = (Point*)malloc(mesh1.vertices.size() * sizeof(Point));
-    mesh_t mesh2 = {bunnymesh2.m_vertices, bunnymesh2.m_normals, bunnymesh2.m_face_v_indices};
-    mesh2.mPoints = (Point*)malloc(mesh2.vertices.size() * sizeof(Point));
 
     auto bunny1 = std::make_shared<ChBody>();
     bunny1->SetPos(ChVector<>(-5, 0, 0));
@@ -312,6 +333,12 @@ int main(int argc, char* argv[]) {
     bunnyasset2->SetMesh(bunnymesh2);
     bunny2->AddAsset(bunnyasset2);
 
+    // In this example the two meshes are the same, the offsets are just different
+    mesh_t mesh1 = {bunnymesh.m_vertices, bunnymesh.m_normals, bunnymesh.m_face_v_indices, *bunny1};
+    mesh1.mPoints = (Point*)malloc(mesh1.vertices.size() * sizeof(Point));
+    mesh_t mesh2 = {bunnymesh2.m_vertices, bunnymesh2.m_normals, bunnymesh2.m_face_v_indices, *bunny2};
+    mesh2.mPoints = (Point*)malloc(mesh2.vertices.size() * sizeof(Point));
+
     // Complete visualization asset construction.
     application.AssetBindAll();
     application.AssetUpdateAll();
@@ -319,7 +346,7 @@ int main(int argc, char* argv[]) {
     // Create the contact manager.
     ContactManager manager(&system);
 
-    double timestep = .05;
+    double timestep = .01;
     // Simulation loop.
     application.SetStepManage(true);
     application.SetTimestep(timestep);
@@ -333,40 +360,40 @@ int main(int argc, char* argv[]) {
         application.DrawAll();
 
         sw.start();
-        int ncontacts = mesh_t_compute_contacts_fast(mesh1, mesh2, ChFrame<>(*bunny1), ChFrame<>(*bunny2));
+        int ncontacts = mesh_t_compute_contacts_fast(mesh1, mesh2);
         sw.stop();
         double my_time = sw.count();
-
-        // Advance dynamics.
-        application.DoStep();
 
         // sw.start();
         // int ncontacts1 = mesh_t_compute_contacts_faster(mesh1, mesh2, ChFrame<>(*bunny1), ChFrame<>(*bunny2));
         // sw.stop();
         // double my_time1 = sw.count();
-
+        //
         // if (ncontacts != ncontacts1) {
         //     std::cerr << "dammit" << std::endl;
         //     exit(1);
         // }
+
+        // Advance dynamics.
+        application.DoStep();
+
         // Process current collisions and report number of contacts on a few bodies.
         manager.Process();
         if (timestep == .05 && (bunny2->GetPos() - bunny1->GetPos()).x() < 7.2) {
             timestep = .005;
             application.SetTimestep(timestep);
         }
+        auto narrow = system.GetTimerCollisionNarrow();
+        auto broad = system.GetTimerCollisionBroad();
+        auto chrono_time = narrow + broad;
 
         std::cout << "offset is " << (bunny2->GetPos() - bunny1->GetPos()).x() << std::endl;
+        std::cout << "time ratio is " << my_time / (chrono_time * 1000) << std::endl;
         if (manager.GetNcontacts(bunny1) != 0 || ncontacts != 0) {
-            auto narrow = system.GetTimerCollisionNarrow();
-            auto broad = system.GetTimerCollisionBroad();
-            auto chrono_time = narrow + broad;
+            std::cout << "fast took " << my_time / 1000 << " s !" << std::endl;
+            std::cout << "chrono time is " << chrono_time << " s !" << std::endl;
             std::cout << "narrow is " << narrow << " broad is " << broad << std::endl;
-            std::cout << "chrono time is " << chrono_time << " s!" << std::endl;
-
-            std::cout << "fast took " << my_time / 1000 << " s!" << std::endl;
             // std::cout << "new took " << my_time1 / 1000 << " s!" << std::endl;
-            std::cout << "time ratio is " << my_time / (chrono_time * 1000) << std::endl;
             printf("ncontacts is %d\n", ncontacts);
             std::cout << "Time: " << system.GetChTime();
             std::cout << "   bunny1: " << manager.GetNcontacts(bunny1);
